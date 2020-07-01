a38f131d21c9db291f523d9deafd91f305cb2f3b
Prashant Shetage <shetageprashant@gmail.com>
2020-07-01 23:49:46 +0530

    Commented advanced arguments and variables

--- aks.tf
+++ aks.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   dns_prefix          = var.dns_prefix
   kubernetes_version  = var.kubernetes_version
 
-  role_based_access_control {
+  /*    role_based_access_control {
     enabled = var.role_based_access_control.enabled
     azure_active_directory {
       client_app_id     = var.role_based_access_control.client_app_id
@@ -18,18 +18,16 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   linux_profile {
     admin_username = var.admin_username
-
     ssh_key {
-      # remove any new lines using the replace interpolation function
       key_data = replace(var.admin_public_ssh_key, "\n", "")
     }
-  }
+  } */
 
   default_node_pool {
     name                  = var.default_node_pool.name
     vm_size               = var.default_node_pool.vm_size
     availability_zones    = var.default_node_pool.availability_zones
-    enable_auto_scaling   = var.default_node_pool.enable_auto_scaling
+    enable_auto_scaling   = var.default_node_pool_scaling.enable_auto_scaling
     enable_node_public_ip = var.default_node_pool.enable_node_public_ip
     max_pods              = var.default_node_pool.max_pods
     node_labels           = var.default_node_pool.node_labels
@@ -37,22 +35,12 @@ resource "azurerm_kubernetes_cluster" "main" {
     os_disk_size_gb       = var.default_node_pool.os_disk_size_gb
     type                  = var.default_node_pool.type
     vnet_subnet_id        = var.default_node_pool.vnet_subnet_id
-    min_count             = var.default_node_pool.min_count
-    max_count             = var.default_node_pool.max_count
     node_count            = var.default_node_pool.node_count
+    orchestrator_version  = var.default_node_pool.orchestrator_version
     tags                  = var.default_node_pool.tags
-  }
 
-  service_principal {
-    client_id     = var.service_principal_client_id
-    client_secret = var.service_principal_client_secret
-  }
-
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = var.log_analytics_workspace_id
-    }
+    min_count = var.default_node_pool_scaling.enable_auto_scaling ? var.default_node_pool_scaling.min_count : null
+    max_count = var.default_node_pool_scaling.enable_auto_scaling ? var.default_node_pool_scaling.max_count : null
   }
 
   network_profile {
@@ -62,16 +50,33 @@ resource "azurerm_kubernetes_cluster" "main" {
     docker_bridge_cidr = var.network_profile.docker_bridge_cidr
     pod_cidr           = var.network_profile.pod_cidr
     service_cidr       = var.network_profile.service_cidr
+    outbound_type      = var.network_profile.outbound_type
     load_balancer_sku  = var.network_profile.load_balancer_sku
   }
+
+  service_principal {
+    client_id     = var.service_principal.client_id
+    client_secret = var.service_principal.client_secret
+  }
+
+  /* addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = var.log_analytics_workspace_id
+    }
+  } */
 
   tags       = merge(var.resource_tags, var.deployment_tags)
   depends_on = [var.it_depends_on]
 
   lifecycle {
     ignore_changes = [
+      #addon_profile[0].kube_dashboard,
+      default_node_pool[0].node_count,
+      default_node_pool[0].max_count,
+      default_node_pool[0].min_count,
+      default_node_pool[0].tags,
       tags,
-      XXXXXXXXXXXXXXX
     ]
   }
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-
-// AKS cluster
+// Required Variables
+//**********************************************************************************************
 variable "resource_group_name" {
   type        = string
   description = "(Required) Specifies the Resource Group where the Managed Kubernetes Cluster should exist"
@@ -10,12 +10,71 @@ variable "location" {
   description = "(Required) The location where the Managed Kubernetes Cluster should be created"
 }
 
+variable "name" {
+  type        = string
+  description = "(Required) The name of the Managed Kubernetes Cluster to create"
+}
+
 variable "dns_prefix" {
   type        = string
   description = "(Required) DNS prefix specified when creating the managed cluster"
 }
 
-// Addon Profile
+variable "default_node_pool" {
+  description = "A default_node_pool block, see terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#default_node_pool"
+  type = object({
+    name                  = string       # (Required) The name which should be used for the default Kubernetes Node Pool
+    vm_size               = string       #(Required) The size of the Virtual Machine, such as Standard_DS2_v2
+    availability_zones    = list(string) #(Optional) A list of Availability Zones across which the Node Pool should be spread
+    enable_node_public_ip = bool         #(Optional) Should nodes in this Node Pool have a Public IP Address? Defaults to false
+    max_pods              = number       #Optional) The maximum number of pods that can run on each agent
+    node_labels           = map(string)  #(Optional) A map of Kubernetes labels which should be applied to nodes in the Default Node Pool
+    node_taints           = list(string) #(Optional) A list of Kubernetes taints which should be applied to nodes in the agent pool
+    os_disk_size_gb       = number       #(Optional) The size of the OS Disk which should be used for each agent in the Node Pool
+    type                  = string       #(Optional) The type of Node Pool which should be created. Possible values are AvailabilitySet and VirtualMachineScaleSets
+    vnet_subnet_id        = string       #(Optional) The ID of a Subnet where the Kubernetes Node Pool should exist
+    node_count            = number       #(Optional) The initial number of nodes which should exist in this Node Pool
+    orchestrator_version  = string       #(Optional) Version of Kubernetes used for the Agents
+    tags                  = map(string)  #(Optional) A mapping of tags to assign to the Node Pool
+
+  })
+  default = {
+    name                  = "nodepool"
+    vm_size               = "standard_f2"
+    availability_zones    = null #[]
+    enable_node_public_ip = false
+    max_pods              = null
+    node_labels           = null #{}
+    node_taints           = null #[]
+    os_disk_size_gb       = null
+    type                  = "VirtualMachineScaleSets"
+    vnet_subnet_id        = null
+    node_count            = null
+    orchestrator_version  = null
+    tags                  = null #{}
+  }
+}
+
+variable "default_node_pool_scaling" {
+  type = object({
+    enable_auto_scaling = bool   #(Optional) Should the Kubernetes Auto Scaler be enabled for this Node Pool? Defaults to false
+    min_count           = number #(Required) The maximum number of nodes which should exist in this Node Pool
+    max_count           = number #(Required) The minimum number of nodes which should exist in this Node Pool.
+  })
+  description = "(Optional) Should the Kubernetes Auto Scaler be enabled for the Node Pool?"
+  default = {
+    enable_auto_scaling = false
+    min_count           = null
+    max_count           = null
+  }
+}
+//**********************************************************************************************
+
+
+
+// Optional Variables
+//**********************************************************************************************
+### Addon Profile ###
 variable "addon_profile_aci_connector_linux" {
   type = object({
     enabled     = bool   #(Required) Is the virtual node addon enabled
@@ -27,6 +86,7 @@ variable "addon_profile_aci_connector_linux" {
     subnet_name = null
   }
 }
+# Azure policy is in public preview: https://docs.microsoft.com/en-gb/azure/governance/policy/concepts/policy-for-kubernetes
 variable "addon_profile_azure_policy" {
   type = object({
     enabled = bool #(Required) Is the Azure Policy for Kubernetes Add On enabled?
@@ -67,13 +127,141 @@ variable "addon_profile_oms_agent" {
   }
 }
 
-varibale "api_server_authorized_ip_ranges" {
+# Security
+variable "enable_pod_security_policy" {
+  type        = bool
+  description = "(Optional) Whether Pod Security Policies are enabled. Note that this also requires role based access control to be enabled"
+  default     = false
+}
+variable "disk_encryption_set_id" {
+  type        = string
+  description = "(Optional) The ID of the Disk Encryption Set which should be used for the Nodes and Volumes"
+  default     = null
+}
+variable "api_server_authorized_ip_ranges" {
   type        = list(string)
   description = "(Optional) The IP ranges to whitelist for incoming traffic to the masters"
   default     = []
 }
 
-// Auto Scaler
+
+# Authentication and Authorization
+variable "role_based_access_control" {
+  type = object({
+    enabled = bool                    #(Required) Is Role Based Access Control Enabled
+    azure_active_directory = object({ # (Optional) An azure_active_directory block
+      client_app_id     = string
+      server_app_id     = string
+      server_app_secret = string
+      tenant_id         = string
+    })
+  })
+  description = "(Optional) A role_based_access_control block"
+  default = {
+    enabled = false
+    azure_active_directory = {
+      client_app_id     = null
+      server_app_id     = null
+      server_app_secret = null
+      tenant_id         = null
+    }
+  }
+}
+variable "identity" {
+  type = object({
+    type = string #(Required) The type of identity used for the managed cluster
+  })
+  description = "(Optional) Managed Identity to interact with Azure APIs"
+  default = {
+    type = "SystemAssigned"
+  }
+}
+variable "service_principal" {
+  type = object({
+    client_id     = string #(Required) The Client ID for the Service Principal
+    client_secret = string #(Required) The Client Secret for the Service Principal.
+  })
+  description = "(Optional) Service principle to interact with Azure APIs"
+  default = {
+    client_id     = null
+    client_secret = null
+  }
+}
+variable "linux_profile" {
+  type = object({
+    admin_username = string #(Required) The Admin Username for the Cluster
+    ssh_key = object({
+      key_data = string #(Required) The Public SSH Key used to access the cluster
+    })
+  })
+  description = "(Optional) SSH authentication parameter values"
+  default = {
+    admin_username = null
+    ssh_key = {
+      key_data = null
+    }
+  }
+}
+variable "windows_profile" {
+  type = object({
+    admin_username = string #(Required) The Admin Username for Windows VMs
+    admin_password = string #(Required) The Admin Password for Windows VMs.
+  })
+  description = "(Optional) Windows Profile"
+  default = {
+    admin_username = null
+    admin_password = null
+  }
+}
+
+
+# Networking
+variable "network_profile" {
+  type = object({
+    network_plugin     = string #(Required) Network plugin to use for networking
+    network_policy     = string #(Optional) Sets up network policy to be used with Azure CNI
+    dns_service_ip     = string #(Optional) IP address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns)
+    docker_bridge_cidr = string #(Optional) IP address (in CIDR notation) used as the Docker bridge IP address on nodes
+    pod_cidr           = string #(Optional) The CIDR to use for pod IP addresses. This field can only be set when network_plugin is set to kubenet
+    service_cidr       = string #(Optional) The Network Range used by the Kubernetes service
+    outbound_type      = string #(Optional) The outbound (egress) routing method which should be used for this Kubernetes Cluster
+    load_balancer_sku  = string #(Optional) Specifies the SKU of the Load Balancer used for this Kubernetes Cluster
+    #load_balancer_profile = object({})
+  })
+  description = "(Optional) Variables defining the AKS network profile config"
+  default = {
+    network_plugin     = null
+    network_policy     = null
+    dns_service_ip     = null
+    docker_bridge_cidr = null
+    pod_cidr           = null
+    service_cidr       = null
+    outbound_type      = null
+    load_balancer_sku  = null
+  }
+}
+variable "private_cluster_enabled" {
+  type        = bool
+  description = "(Optional) Should this Kubernetes Cluster have it's API server only exposed on internal IP addresses?"
+  default     = false
+}
+
+# Management
+variable "kubernetes_version" {
+  type        = string
+  description = "(Optional) Version of Kubernetes specified when creating the AKS managed cluster"
+  default     = "1.15.11"
+}
+variable "sku_tier" {
+  type        = string
+  description = "(Optional) The SKU Tier that should be used for this Kubernetes Cluster"
+  default     = "Free"
+}
+variable "node_resource_group" {
+  description = "(Optional) The name of the Resource Group where the Kubernetes Nodes should exist"
+  default     = null
+}
+# Auto Scaler
 variable "auto_scaler_profile" {
   type = object({
     balance_similar_node_groups      = bool   #Detect similar node groups and balance the number of nodes between them
@@ -100,157 +288,6 @@ variable "auto_scaler_profile" {
   }
 }
 
-variable "rbac_enabled" {
-  description = "Boolean to enable or disable role-based access control"
-  type        = bool
-  default     = true
-}
-
-
-
-variable "CLIENT_ID" {
-  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
-  type        = string
-}
-
-variable "CLIENT_SECRET" {
-  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
-  type        = string
-}
-
-variable "admin_username" {
-  default     = "azureuser"
-  description = "The username of the local administrator to be created on the Kubernetes cluster"
-  type        = string
-}
-
-variable "log_analytics_workspace_sku" {
-  description = "The SKU (pricing level) of the Log Analytics workspace. For new subscriptions the SKU should be set to PerGB2018"
-  default     = "PerGB2018"
-  type        = string
-}
-
-variable "log_retention_in_days" {
-  description = "The retention period for the logs in days"
-  default     = 30
-  type        = number
-}
-
-
-
-variable "public_ssh_key" {
-  description = "A custom ssh key to control access to the AKS cluster"
-  default     = ""
-  type        = string
-}
-
-variable "agent_pool_profile" {
-  description = "An agent_pool_profile block, see terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#agent_pool_profile"
-  type        = list(any)
-  default = [{
-    name            = "nodepool"
-    count           = 1
-    vm_size         = "standard_f2"
-    os_type         = "Linux"
-    agents_count    = 2
-    os_disk_size_gb = 50
-  }]
-}
-
-variable "default_node_pool" {
-  description = "A default_node_pool block, see terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#default_node_pool"
-  type = object({
-    name                  = string       # (Required) The name which should be used for the default Kubernetes Node Pool
-    vm_size               = string       #(Required) The size of the Virtual Machine, such as Standard_DS2_v2
-    availability_zones    = list(string) #(Optional) A list of Availability Zones across which the Node Pool should be spread
-    enable_node_public_ip = bool         #(Optional) Should nodes in this Node Pool have a Public IP Address? Defaults to false
-    max_pods              = number       #Optional) The maximum number of pods that can run on each agent
-    node_labels           = map(string)  #(Optional) A map of Kubernetes labels which should be applied to nodes in the Default Node Pool
-    node_taints           = list(string) #(Optional) A list of Kubernetes taints which should be applied to nodes in the agent pool
-    os_disk_size_gb       = number       #(Optional) The size of the OS Disk which should be used for each agent in the Node Pool
-    type                  = string       #(Optional) The type of Node Pool which should be created. Possible values are AvailabilitySet and VirtualMachineScaleSets
-    vnet_subnet_id        = string       #(Optional) The ID of a Subnet where the Kubernetes Node Pool should exist
-    node_count            = number       #(Optional) The initial number of nodes which should exist in this Node Pool
-    orchestrator_version  = string       #(Optional) Version of Kubernetes used for the Agents
-    tags                  = map(string)  #(Optional) A mapping of tags to assign to the Node Pool
-
-  })
-  default = {
-    name                  = "nodepool"
-    vm_size               = "standard_f2"
-    availability_zones    = null #[]
-    enable_node_public_ip = false
-    max_pods              = null
-    node_labels           = null #{}
-    node_taints           = null #[]
-    os_disk_size_gb       = null
-    type                  = "VirtualMachineScaleSets"
-    vnet_subnet_id        = null
-    node_count            = null
-    tags                  = null #{}
-  }
-}
-
-variable "default_node_pool_scaling" {
-  type = object({
-    enable_auto_scaling = bool   #(Optional) Should the Kubernetes Auto Scaler be enabled for this Node Pool? Defaults to false
-    min_count           = number #(Required) The maximum number of nodes which should exist in this Node Pool
-    max_count           = number #(Required) The minimum number of nodes which should exist in this Node Pool.
-  })
-  description = "(Optional) Should the Kubernetes Auto Scaler be enabled for the Node Pool?"
-  default = {
-    enable_auto_scaling = false
-    min_count           = null
-    max_count           = null
-  }
-}
-
-
-
-
-# variable "aks_ignore_changes" {
-#   description = "lifecycle.aks_ignore_changes to ignore"
-#   type        = list(string)
-#   default     = [""]
-# }
-
-variable "network_profile" {
-  description = "Variables defining the AKS network profile config"
-  type = object({
-    network_plugin     = string
-    network_policy     = string
-    dns_service_ip     = string
-    docker_bridge_cidr = string
-    pod_cidr           = string
-    service_cidr       = string
-    load_balancer_sku  = string
-  })
-  default = {
-    network_plugin     = "kubenet"
-    network_policy     = ""
-    dns_service_ip     = ""
-    docker_bridge_cidr = ""
-    pod_cidr           = ""
-    service_cidr       = ""
-    load_balancer_sku  = "Basic"
-  }
-}
-
-variable "tags" {
-  default     = {}
-  description = "Any tags that should be present on resources"
-  type        = map(string)
-}
-
-
-
-
-
-
-
-// Optional Variables
-//**********************************************************************************************
-
 variable "aks_suffix" {
   type        = string
   description = "(Optional) Suffix for AKS cluster name"
@@ -263,57 +300,34 @@ variable "aks_prefix" {
   default     = ""
 }
 
-variable "kubernetes_version" {
-  type        = string
-  description = "(Optional) Version of Kubernetes specified when creating the AKS managed cluster"
-  default     = "1.15.11"
-}
-
-variable "role_based_access_control" {
-  type = object({
-    enabled = bool                    #(Required) Is Role Based Access Control Enabled
-    azure_active_directory = object({ # (Optional) An azure_active_directory block
-      client_app_id     = string
-      server_app_id     = string
-      server_app_secret = string
-      tenant_id         = string
-    })
-  })
-  description = "(Optional) A role_based_access_control block"
-  default = {
-    enabled = false
-    azure_active_directory = {
-      client_app_id     = null
-      server_app_id     = null
-      server_app_secret = null
-      tenant_id         = null
-    }
-  }
-}
-
-
 variable "resource_tags" {
   type        = map(string)
-  description = "(Optional) Tags for resources"
+  description = "(Optional) Tags for the resources"
   default     = {}
 }
+
 variable "deployment_tags" {
   type        = map(string)
-  description = "(Optional) Tags for deployment"
+  description = "(Optional) Additional Tags for the deployment"
   default     = {}
 }
+
 variable "it_depends_on" {
   type        = any
   description = "(Optional) To define explicit dependencies if required"
   default     = null
 }
-//**********************************************************************************************
+
+variable "timeout" {
+  type        = string
+  description = "(Optional) Timeout"
+  default     = "90m"
+}
 
 
 // Local Values
-//**********************************************************************************************
 locals {
-  timeout_duration = "1h"
-  aks_name         = "${var.aks_prefix}${var.aks_suffix}"
+  timeout_duration = var.timeout
+  aks_name         = "${var.aks_prefix}${var.name}${var.aks_suffix}"
 }
 //**********************************************************************************************
