b84faffd35debbcfd3f32062d773af365adcac30
Prashant Shetage <shetageprashant@gmail.com>
2020-09-25 22:40:08 +0530

    Role assignment for CSI Driver with AAD Authentication

---
+++ aks_role_assignments.tf
@@ -0,0 +1,24 @@
+// Fetch the ID of AKS Nodes Resource Group
+data "azurerm_resource_group" "aks_node_rg" {
+  name = azurerm_kubernetes_cluster.aks_cluster.node_resource_group
+}
+
+
+// Role assignment for AKS Managed Identity over the AKS Node Resource Group
+resource "azurerm_role_assignment" "role_assignment_node_rg" {
+  for_each             = var.csi_with_aadpod_id ? var.role_assignment_node_rg : {}
+  scope                = data.azurerm_resource_group.aks_node_rg.id
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].user_assigned_identity_id
+  role_definition_name = each.value.role_definition_name
+  role_definition_id   = each.value.role_definition_id
+}
+
+
+// Additional role assignments for AKS Managed identity
+resource "azurerm_role_assignment" "role_assignment_others" {
+  for_each             = var.csi_with_aadpod_id ? var.role_assignment_others : {}
+  scope                = each.value.scope
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].user_assigned_identity_id
+  role_definition_name = each.value.role_definition_name
+  role_definition_id   = each.value.role_definition_id
+}
